f6090b8ac099abbefa6b1d289d1f9921675b689e
joyceh14 <whuang152@my.bcit.ca>
2025-02-21 13:24:22 -0800

    modified baseUrl in blazor

--- BlazorStudentApp/Program.cs
+++ BlazorStudentApp/Program.cs
@@ -6,6 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071/api/") });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071/api/") });
+builder.Services.AddScoped(sp => new HttpClient 
+{ 
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+});
+
 
 await builder.Build().RunAsync();
